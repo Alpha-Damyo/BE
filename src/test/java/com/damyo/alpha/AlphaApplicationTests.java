a3a7e5107a88476be74c7250d92356eb86d4be5b
ji hwan <jjhinu1004@gmail.com>
2024-03-03 12:49:14 +0900

    spring init

---
+++ src/test/java/com/damyo/alpha/AlphaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.damyo.alpha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlphaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
