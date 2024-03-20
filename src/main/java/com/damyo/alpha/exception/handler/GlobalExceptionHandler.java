0dace9b3627319df632e2ae8ac16dc9da0abca5f
BlueBerrySoda <opace2341@kookmin.ac.kr>
2024-03-20 18:49:11 +0900

    feat: create Exception Package

---
+++ src/main/java/com/damyo/alpha/exception/handler/GlobalExceptionHandler.java
@@ -0,0 +1,7 @@
+package com.damyo.alpha.exception.handler;
+
+import jdk.jshell.spi.ExecutionControl;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+}
