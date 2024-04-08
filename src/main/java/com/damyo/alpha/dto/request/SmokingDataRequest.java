934b13d2067adfa694552cd37931238764035134
BlueBerrySoda <opace2341@kookmin.ac.kr>
2024-04-08 10:49:05 +0900

    fix: Request DTO 에서 DB Entity 객체를 요구하는 버그 수정

--- src/main/java/com/damyo/alpha/dto/request/SmokingDataRequest.java
+++ src/main/java/com/damyo/alpha/dto/request/SmokingDataRequest.java
@@ -4,9 +4,10 @@ import com.damyo.alpha.domain.SmokingArea;
 import com.damyo.alpha.domain.User;
 
 import java.time.LocalDateTime;
+import java.util.UUID;
 
 public record SmokingDataRequest(
-        User user,
+        String email,
         LocalDateTime creatdAt,
         SmokingArea smokingArea
 ) {
--- src/main/java/com/damyo/alpha/service/SmokingDataService.java
+++ src/main/java/com/damyo/alpha/service/SmokingDataService.java
@@ -1,9 +1,11 @@
 package com.damyo.alpha.service;
 
 import com.damyo.alpha.domain.SmokingData;
+import com.damyo.alpha.domain.User;
 import com.damyo.alpha.dto.request.SmokingDataRequest;
 import com.damyo.alpha.dto.request.SmokingDataListRequest;
 import com.damyo.alpha.repository.SmokingDataRepository;
+import com.damyo.alpha.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -14,11 +16,13 @@ import java.util.List;
 public class SmokingDataService {
 
     private final SmokingDataRepository smokingDataRepository;
+    private final UserRepository userRepository;
 
     public void addSmokingData(SmokingDataListRequest dataListRequest) {
         List<SmokingDataRequest> smokingDataRequests = dataListRequest.dataRequests();
         for(SmokingDataRequest dataRequest : smokingDataRequests) {
-            smokingDataRepository.save(SmokingData.builder().user(dataRequest.user()).createdAt(dataRequest.creatdAt()).smokingArea(dataRequest.smokingArea()).build());
+            User user = userRepository.findUserByEmail(dataRequest.email()).get();
+            smokingDataRepository.save(SmokingData.builder().user(user).createdAt(dataRequest.creatdAt()).smokingArea(dataRequest.smokingArea()).build());
         }
     }
 
