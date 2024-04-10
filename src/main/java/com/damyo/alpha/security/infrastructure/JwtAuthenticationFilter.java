1e01c0ba452096a5bdbe5b9819f5b0fa5e8e3941
ji hwan <jjhinu1004@gmail.com>
2024-04-10 22:30:36 +0900

    로깅 코드 삭제

--- src/main/java/com/damyo/alpha/security/infrastructure/JwtAuthenticationFilter.java
+++ src/main/java/com/damyo/alpha/security/infrastructure/JwtAuthenticationFilter.java
@@ -37,18 +37,14 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         String token = jwtProvider.resolveToken(request);
-        log.info(request.getRequestURI());
         try {
             String email = jwtProvider.validateTokenAndGetEmail(token);
-            log.info(email);
             Authentication authentication = jwtProvider.createAuthentication(email);
             SecurityContextHolder.getContext().setAuthentication(authentication);
         } catch (SecurityException e) {
             request.setAttribute("exception", new AuthException(SIGNATURE_NOT_FOUND));
-            log.info(e.getMessage());
         } catch (SignatureException e) {
             request.setAttribute("exception", new AuthException(SIGNATURE_INVALID));
-            log.info(e.getMessage());
         } catch (MalformedJwtException e) {
             request.setAttribute("exception", new AuthException(MALFORMED_TOKEN));
         } catch (ExpiredJwtException e) {
@@ -57,10 +53,8 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
             request.setAttribute("exception", new AuthException(UNSUPPORTED_TOKEN));
         } catch (IllegalArgumentException e) {
             request.setAttribute("exception", new AuthException(INVALID_TOKEN));
-            log.info(e.getMessage());
         } catch (UsernameNotFoundException e) {
             request.setAttribute("exception", new AuthException(EMAIL_NOT_FOUND));
-            log.info(e.getMessage());
         } catch (Exception e) {
             request.setAttribute("exception", new AuthException(INTERNAL_SERVER_ERROR));
         }
