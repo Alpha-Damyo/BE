0f8b5bcf07c02cbef878b94f0b30f4b07575ffe5
BlueBerrySoda <opace2341@kookmin.ac.kr>
2024-04-15 01:43:18 +0900

    feat: #41-시간대별 통계 기능 수정

--- src/main/java/com/damyo/alpha/dto/response/HourlyStatisticsResponse.java
+++ src/main/java/com/damyo/alpha/dto/response/HourlyStatisticsResponse.java
@@ -1,4 +1,13 @@
 package com.damyo.alpha.dto.response;
 
-public record HourlyStatisticsResponse() {
+public record HourlyStatisticsResponse(
+        Integer hour0To3,
+        Integer hour3To6,
+        Integer hour6To9,
+        Integer hour9To12,
+        Integer hour12To15,
+        Integer hour15To18,
+        Integer hour18To21,
+        Integer hour21To0
+) {
 }
--- src/main/java/com/damyo/alpha/service/SmokingDataService.java
+++ src/main/java/com/damyo/alpha/service/SmokingDataService.java
@@ -53,10 +53,40 @@ public class SmokingDataService {
     private HourlyStatisticsResponse getHourlyStatistics() {
         List<SmokingData> dataList = smokingDataRepository.findSmokingDataByCreateAt(LocalDateTime.now().minusYears(1), LocalDateTime.now());
 
+        Integer count0To3 = 0;
+        Integer count3To6 = 0;
+        Integer count6To9 = 0;
+        Integer count9To12 = 0;
+        Integer count12To15 = 0;
+        Integer count15To18 = 0;
+        Integer count18To21 = 0;
+        Integer count21To0 = 0;
+
+        for(SmokingData data : dataList){
+            int h = data.getCreatedAt().getHour();
+            if(0 <= h && h < 3) count0To3 += 1;
+            else if(3 <= h && h < 6) count3To6 += 1;
+            else if(6 <= h && h < 9) count6To9 += 1;
+            else if(9 <= h && h < 12) count9To12 += 1;
+            else if(12 <= h && h < 15) count12To15 += 1;
+            else if(15 <= h && h < 18) count15To18 += 1;
+            else if(18 <= h && h < 21) count18To21 += 1;
+            else if(21 <= h) count21To0 += 1;
+        }
+
+        return new HourlyStatisticsResponse(count0To3, count3To6, count6To9, count9To12, count12To15, count15To18, count18To21, count21To0);
     }
 
     //TODO 일단위 평균 통계 기능
     private DailyStatisticsResponse getDailyStatistics() {
+        List<SmokingData> dataList = smokingDataRepository.findSmokingDataByCreateAt(LocalDateTime.now().minusYears(1), LocalDateTime.now());
+        Integer[] days = new Integer[32];
+
+        for(SmokingData data : dataList) {
+            int d = data.getCreatedAt().getDayOfMonth();
+            days[d] += 1;
+        }
+
 
     }
 
