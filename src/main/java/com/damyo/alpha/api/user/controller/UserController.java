b7fbba5ac5bb664179c66d82476d147a2a2214bc
Jihwan Jung <jjhinu1004@gmail.com>
2024-05-28 18:44:30 +0900

    fix: multipart 부분에 schema -> parameter로 변경

--- src/main/java/com/damyo/alpha/api/user/controller/UserController.java
+++ src/main/java/com/damyo/alpha/api/user/controller/UserController.java
@@ -6,6 +6,8 @@ import com.damyo.alpha.api.user.controller.dto.UserResponse;
 import com.damyo.alpha.api.user.service.UserService;
 import com.damyo.alpha.global.exception.error.ErrorResponse;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
@@ -63,7 +65,8 @@ public class UserController {
             @ApiResponse(responseCode = "A104", description = "토큰이 유효하지 않을 경우", content = @Content(schema = @Schema(implementation = ErrorResponse.class)))
     })
     @PutMapping(value = "/update/profile", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    public ResponseEntity<?> updateProfile(@AuthenticationPrincipal UserDetailsImpl details, @Schema(description = "변경할 이미지 파일") @RequestPart MultipartFile profile) {
+    public ResponseEntity<?> updateProfile(@Parameter(description = "변경할 프로필 사진", in = ParameterIn.DEFAULT) @RequestPart(value = "image") MultipartFile profile,
+                                           @AuthenticationPrincipal UserDetailsImpl details) {
         String profileUrl = s3ImageService.upload(profile);
         String prevUrl = userService.updateProfile(details, profileUrl);
         s3ImageService.deleteImageFromS3(prevUrl);
