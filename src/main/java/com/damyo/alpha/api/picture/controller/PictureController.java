7046a7b3f5d1e1ec9a2dfaeda8bf0bbbb39719fc
BlueBerrySoda <opace2341@kookmin.ac.kr>
2024-05-28 18:54:54 +0900

    refactor: 실제 연동시 발생하는 버그 수정

--- src/main/java/com/damyo/alpha/api/picture/controller/PictureController.java
+++ src/main/java/com/damyo/alpha/api/picture/controller/PictureController.java
@@ -16,6 +16,7 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -70,7 +71,7 @@ public class PictureController {
         return ResponseEntity.ok().body(pictureList);
     }
 
-    @PostMapping("/upload")
+    @PostMapping(value = "/upload", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     @Operation(summary="사진 업로드", description = "사진을 업로드 합니다.")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "사진 업로드에 성공하였습니다.", content = @Content(mediaType = "application/json")),
