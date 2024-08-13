76d7e7f9de0f76fc5228e9954e8a0d1149b717e8
ji hwan <jjhinu1004@gmail.com>
2024-08-13 21:58:00 +0900

    chore: 기본 서버 url 변경

--- src/main/java/com/damyo/alpha/global/config/SwaggerConfig.java
+++ src/main/java/com/damyo/alpha/global/config/SwaggerConfig.java
@@ -5,6 +5,7 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -24,6 +25,7 @@ public class SwaggerConfig {
                         .bearerFormat("token"));
 
         return new OpenAPI()
+                .addServersItem(new Server().url("/"))
                 .info(apiInfo())
                 .components(components);
     }
